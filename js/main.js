69c6a3d53348014deb4c283d0c4b9e508688be08
sahadatpro <sahadatsays@gmail.com>
2019-04-28 12:25:07 +0600

    Design complete my personal portfolio template

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
 $(document).ready(function () {
+
+    // navigation
+    $(window).scroll(function () {
+        let position = $(this).scrollTop();
+        if(position >= 500) {
+            $("#custom-nabvar").css("height", "60px");
+        }else {
+            $("#custom-nabvar").css("height", "90px");
+        }
+    });
+
     $(".nav-btn").on("click", function(){
         $(".nav-btn").toggleClass("change");
      });
      
+
+    //  Project Gallery
+    $(".p-item").click(function () {
+        let value = $(this).attr("data-filter");
+        
+        if(value === "all") {
+            $(".filter").show();
+        } else {
+            $(".filter").not("."+value).hide();
+            $(".filter").filter("."+value).show();
+        }
+        $(this).addClass("p-active").siblings().removeClass("p-active");
+    });
 });
